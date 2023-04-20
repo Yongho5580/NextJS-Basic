dcece1af94938a7d8a1db884555ac87a31db1fbe
Yongho5580 <70843139+Yongho5580@users.noreply.github.com>
2023-04-20 17:03:23 +0900

    finish

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,53 +1,64 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
-const API_KEY = `97254b47f45871505d334f0e8bd1b741`;
-
 interface MovieProps {
-  id: number;
-  original_title: string;
+  results: Array<{
+    id: number;
+    original_title: string;
+  }>;
 }
 
-export default function Home() {
-  const [movies, setMovies] = useState<Array<MovieProps>>();
-  useEffect(() => {
-    // IIFE는 즉시 실행 함수이며 단 한번만 실행될 때 사용된다. 첫번째 괄호는 익명함수를, 두번째 괄호는 즉시 실행을 의미한다. 따라서 익명함수를 정의하고 즉시실행에
-    // 불필요한 전역변수를 렉시컬 스코프에 저장하지 않게 된다.
-    (async () => {
-      const { results } = await (await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)).json();
-      console.log(movies);
-      setMovies(results);
-    })();
-  }, []);
+export default function Home({ results }: MovieProps) {
+  const router = useRouter();
+  const onClick = (id: number, title: string) => {
+    router.push(`/movies/${title}/${id}`);
+  };
   return (
     <div className="container">
-    {!movies && <h4>Loading...</h4>}
-    {movies?.map((movie) => (
-      <div className="movie" key={movie.id}>
-        <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-        <h4>{movie.original_title}</h4>
-      </div>
-    ))}
-    <style jsx>{`
-      .container {
-        display: grid;
-        grid-template-columns: 1fr 1fr;
-        padding: 20px;
-        gap: 20px;
-      }
-      .movie img {
-        max-width: 100%;
-        border-radius: 12px;
-        transition: transform 0.2s ease-in-out;
-        box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
-      }
-      .movie:hover img {
-        transform: scale(1.05) translateY(-10px);
-      }
-      .movie h4 {
-        font-size: 18px;
-        text-align: center;
-      }
-    `}</style>
-  </div>
+      {results?.map((movie) => (
+        <div onClick={() => onClick(movie.id, movie.original_title)} className="movie" key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+          <h4>
+            <Link
+              legacyBehavior
+              href={`/movies/${movie.original_title}/${movie.id}`}
+            >
+              <a>{movie.original_title}</a>
+            </Link>
+          </h4>
+        </div>
+      ))}
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
+    </div>
   );
+}
+
+export async function getServerSideProps() {
+  const { results } = await (await fetch(`http://localhost:3000/api/movies`)).json();
+  return {
+    props: {
+      results,
+    },
+  };
 }
---
+++ pages/movies/all.tsx
@@ -0,0 +1,7 @@
+export default function All() {
+  return (
+    <div>
+      <h1>All Movies</h1>
+    </div>
+  );
+}
